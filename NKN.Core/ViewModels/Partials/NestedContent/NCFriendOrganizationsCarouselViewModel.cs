83d1f0b73036004ee34bb902b82b3cd669659139
Zeljko Bal <z.bal@vegaitsourcing.rs>
2019-10-05 16:51:08 +0200

    Friends in progress

---
+++ NKN.Core/ViewModels/Partials/NestedContent/NCFriendOrganizationsCarouselViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NKN.Core.ViewModels.Partials.NestedContent
+{
+    public class NCFriendOrganizationsCarouselViewModel// : INestedContentViewModel
+    {
+        /*public NCFriendOrganizationsCarouselViewModel(INestedContentContext<NCrichText> context)
+        {
+            Text = context.NestedContent.Text;
+        }*/
+    }
+}
