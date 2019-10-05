41ed6693387bc1d380f7741b0ab1b3248c9826c0
Ivana Tesanovic <i.tesanovic@vegaitsourcing.rs>
2019-10-05 21:25:41 +0200

    Added MissionCarouselBlock

---
+++ NKN.Core/ViewModels/Partials/NestedContent/MissionCarouselBlockViewModel.cs
@@ -0,0 +1,23 @@
+﻿using NKN.Core.Contexts;
+using NKN.Core.Extensions;
+using NKN.Core.ViewModels.Shared;
+using NKN.Models.Generated;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NKN.Core.ViewModels.Partials.NestedContent
+{
+    public class MissionCarouselBlockViewModel : INestedContentViewModel
+    {
+        public MissionCarouselBlockViewModel(INestedContentContext<MissionCarouselBlock> context)
+        {
+            Title = context.NestedContent.Title;
+            BackgroundImage = (context.NestedContent.BackgroundImage as Image).ToViewModel();
+            Items = context.NestedContent.Items.Select(i => context.WithNestedContent(i).ToViewModel<MissionItemViewModel>());
+        }
+
+        public string Title { get; private set; }
+        public ImageViewModel BackgroundImage { get; private set; }
+        public IEnumerable<MissionItemViewModel> Items { get; private set; }
+    }
+}
