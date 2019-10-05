97fcbff87270288a948b00b1aad22415626432ee
Ivana Tesanovic <i.tesanovic@vegaitsourcing.rs>
2019-10-05 22:01:53 +0200

    Fixed StandardContentPage

--- NKN.Core/ViewModels/Pages/StandardContentPageViewModel.cs
+++ NKN.Core/ViewModels/Pages/StandardContentPageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using NKN.Core.Contexts;
 using NKN.Core.Extensions;
 using NKN.Core.ViewModels.Partials.NestedContent;
+using NKN.Core.ViewModels.Shared;
 using NKN.Models.DocumentTypes;
 using NKN.Models.Generated;
 using System.Collections.Generic;
@@ -12,11 +13,14 @@ namespace NKN.Core.ViewModels.Pages
     {
         public StandardContentPageViewModel(IPageContext<StandardContentPage> context) : base(context)
         {
+            PageTitle = context.Page.PageTitle;
+            BannerImage = (context.Page.BannerImage as Image).ToViewModel();
             //Banner = new BannerViewModel(context.WithComposition(context.Page));
             Items = context.Page.ContentBlocks?.OfType<INestedContent>().Select(m => context.WithNestedContent(m).AsViewModel<INestedContentViewModel>()).ToList();
         }
 
-        //public BannerViewModel Banner { get; }
+        public string PageTitle { get; private set; }
+        public ImageViewModel BannerImage { get; private set; }
         public IList<INestedContentViewModel> Items { get; }
     }
 }
--- NKN.Models/Generated/RCeblock.cs
+++ NKN.Models/Generated/RCeblock.cs
@@ -1,9 +1,4 @@
 ﻿using NKN.Models.DocumentTypes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Umbraco.Core.Models.PublishedContent;
 
 namespace NKN.Models.Generated
