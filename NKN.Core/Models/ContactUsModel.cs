601ef1d287b5f4c1b3b63cc8572fd8d882049d97
Ognjen Stanić <o.stanic@vegaitsourcing.rs>
2019-10-06 02:39:29 +0200

    Fixed email sending business logic

---
+++ NKN.Core/Models/ContactUsModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NKN.Core.Models
+{
+    // TODO: multilingual support for error message. Hint: custom attrbiute
+    public struct ContactUsModel
+    {
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
---
+++ NKN.Core/Services/EmailComposer.cs
@@ -0,0 +1,31 @@
+﻿using FluentEmail.Core;
+using System.Text;
+using Umbraco.Core.Composing;
+
+namespace NKN.Core.Services
+{
+    public class EmailComposer : IDiscoverable
+    {
+        private const string SenderEmailAddress = "test.nkn.test@gmail.com";
+        private const string ReceiverName = "NKN";
+        private const string Subject = "Contact us";
+
+        public IFluentEmail ComposeEmail(Models.ContactUsModel model)
+        {
+            var emailBodyBuilder = new StringBuilder()
+                .Append("Kontaktirao/la nas je ")
+                .Append(model.FullName)
+                .Append(", njegova/njena imejl adresa je ")
+                .Append(model.Email)
+                .Append(" sa porukom:")
+                .AppendLine()
+                .Append(model.Message);
+
+            return Email
+                .From(SenderEmailAddress)
+                .To(SenderEmailAddress, ReceiverName)
+                .Subject(Subject)
+                .Body(emailBodyBuilder.ToString());
+        }
+    }
+}
